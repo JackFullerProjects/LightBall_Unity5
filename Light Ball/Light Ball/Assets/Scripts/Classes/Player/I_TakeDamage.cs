e56e35d0457e0e6ec439da599085498b10c23550
howardife <ifehoward@gmail.com>
2015-03-05 22:40:48 +0000

    Player Base Class Draft
    Player base class added with outline for damage
    controller int added but not set up in player movement yet or inputs

---
+++ Light Ball/Light Ball/Assets/Scripts/Classes/Player/I_TakeDamage.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public interface I_TakeDamage
+{
+	void TakeDamage(int _damage, GameObject _sender);
+}
---
+++ Light Ball/Light Ball/Assets/Scripts/Classes/Player/PlayerBaseClass.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBaseClass : MonoBehaviour, I_TakeDamage
+{
+
+	//class to hold player stats
+	public class PlayerStats
+	{
+		public int health;
+		public int ammo;
+
+		//constructor to initialise above variables
+		public PlayerStats(int _health, int _ammo)
+		{
+			health = _health;
+			ammo = _ammo;
+		}
+	}
+
+	//initialise class and variables with values
+	PlayerStats stats = new PlayerStats (100, 100);
+
+	//controller is assigned here so its easier to assign and not stored in class
+	public int controller;
+
+	//take damage interace 
+	public void TakeDamage(int _damage, GameObject _sender)
+	{
+	}
+
+}
